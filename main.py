0c54a5f95d3693bd665ba2e5f0ef32356bb143f9
Hafsat Abdulhamid <hafsatabdulhamid631@gmail.com>
2026-08-04 07:40:15 +0100

    Stage 4: explored SQLite

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from pydantic import BaseModel
 from typing import Optional
 import sqlite3
 
-def get_db_connection():
-    conn = sqlite3.connect("tasks.db")
-    conn.row_factory = sqlite3.Row
-    return conn
-
 class Task(BaseModel):
     title: str
 
